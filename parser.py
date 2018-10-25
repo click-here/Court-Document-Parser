9ac241c70450cfd1c2e650bcd48a602c5d8008b2
click-here <ragespectate@gmail.com>
2018-10-25 00:23:07 -0400

    can now download to temp file and extract text in memory

--- parser.py
+++ parser.py
@@ -4,6 +4,8 @@ import subprocess
 import nltk
 from io import BytesIO
 import re
+import time
+import tempfile
 from nltk.tokenize import sent_tokenize
 
 url = 'https://www.supremecourt.gov/opinions/09pdf/08-205.pdf'
@@ -40,24 +42,30 @@ class SCOTUSOpinion:
         self.t = BytesIO(r.content)
 
         self.t.seek(0)
-    
-##        try:
-##            with open(self.filepath, 'r') as f:
-##                self.text = f.read()
-##        except IOError:
-##            raise IOError("{} not found".format(self.filename))
 
-##        self.case_name = re.findall('UNITED STATES\n+Syllabus\n.+v.+\n',self.text)[0][:-1].split('\n')[-1]
+        fd, path = tempfile.mkstemp()
+        try:
+            with os.fdopen(fd, 'wb') as tmp:
+
+                tmp.write(self.t.read())
+                print(path)
+                self.download(path)
+        finally:
+            os.remove(path)
+
+        self.case_name = re.findall('UNITED STATES\n+Syllabus\n.+v.+\n',self.text)[0][:-1].split('\n')[-1]
 
     def download(self, pdf_destination):
+        self.lines = []
         proc = subprocess.Popen(['pdftotext', pdf_destination, '-'], stdout=subprocess.PIPE)
         while True:
           line = proc.stdout.readline()
           if line != b'':
-            #the real code does filtering here
-            print(line.rstrip().decode("ISO-8859-1"))
+            self.lines.append(line.rstrip().decode("ISO-8859-1"))
           else:
             break
+
+        self.text = '\n'.join(self.lines)
         
 a = SCOTUSOpinion(url)
 
